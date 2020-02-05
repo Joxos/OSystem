4f22e19bfd0a4784a8cdf928db83805963ba1046
Joxos <xujunhao61@163.com>
2020-02-05 17:44:05 +0800

    Add "KB", "MB" and "GB" sign for files.

--- OSystem.py
+++ OSystem.py
@@ -1,20 +1,30 @@
 import os
+
 while True:
     path = os.path.abspath('.')
-    print(path, " $ ", sep='', end='')
+    print(path, "$ ", end='')
     inputedMessage = input()
     resultList = inputedMessage.rstrip().lstrip().split(" ")
     resultDirection = resultList[0]
     if resultDirection == '':
         pass
     elif resultDirection == "ls":
-        print("_____________________________")
-        print("|      NAME      |   SIZE   |")
-        print("—————————————————————————————")
+        print(" _______________________________")
+        print("|       NAME       |    SIZE    |")
+        print(" ———————————————————————————————")
         for i in os.listdir(os.path.abspath('.')):
-            fileSize = os.path.getsize(os.path.abspath('.')+"\\"+i)
-            print('| {:15s}| {:9s}|'.format(str(i), str(fileSize)))
-        print("—————————————————————————————")
+            if os.path.isdir(os.path.abspath('.')+"\\"+i):
+                print('| {:17s}|   IS DIR   |'.format(i))
+            elif os.path.isfile(os.path.abspath('.')+"\\"+i):
+                fileSize = os.path.getsize(os.path.abspath('.')+"\\"+i)
+                if fileSize >= 1024:
+                    sizeString = '%.2f' % (fileSize/1024)+"KB"
+                elif fileSize >= 1024**2:
+                    sizeString = '%.2f' % (fileSize/(1024**2))+"MB"
+                elif fileSize >= 1024**3:
+                    sizeString = '%.1f' % (fileSize/(1024**3))+"GB"
+                print('| {:17s}| {:11s}|'.format(i, sizeString))
+        print(" ———————————————————————————————")
     elif resultDirection == "exit" or resultDirection == "quit":
         break
     elif resultDirection == "dir":
