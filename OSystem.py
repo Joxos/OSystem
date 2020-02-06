c7d770bb59e67f1a08425cc3c7393c5f025f942d
Joxos <xujunhao61@163.com>
2020-02-06 08:46:31 +0800

    Split "ls"direction and add gitignore

--- OSystem.py
+++ OSystem.py
@@ -1,5 +1,8 @@
 import os
 
+import ls
+
+exitWords = ["exit", "quit"]
 while True:
     path = os.path.abspath('.')
     print(path, "$ ", end='')
@@ -9,23 +12,8 @@ while True:
     if resultDirection == '':
         pass
     elif resultDirection == "ls":
-        print(" _______________________________")
-        print("|       NAME       |    SIZE    |")
-        print(" ———————————————————————————————")
-        for i in os.listdir(os.path.abspath('.')):
-            if os.path.isdir(os.path.abspath('.')+"\\"+i):
-                print('| {:17s}|   IS DIR   |'.format(i))
-            elif os.path.isfile(os.path.abspath('.')+"\\"+i):
-                fileSize = os.path.getsize(os.path.abspath('.')+"\\"+i)
-                if fileSize >= 1024:
-                    sizeString = '%.2f' % (fileSize/1024)+"KB"
-                elif fileSize >= 1024**2:
-                    sizeString = '%.2f' % (fileSize/(1024**2))+"MB"
-                elif fileSize >= 1024**3:
-                    sizeString = '%.1f' % (fileSize/(1024**3))+"GB"
-                print('| {:17s}| {:11s}|'.format(i, sizeString))
-        print(" ———————————————————————————————")
-    elif resultDirection == "exit" or resultDirection == "quit":
+        ls.ls(path)
+    elif resultDirection in exitWords:
         break
     elif resultDirection == "dir":
         for i in os.listdir(os.path.abspath('.')):
